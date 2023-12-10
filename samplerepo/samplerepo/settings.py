c3869eca9f91acfa8be49697cd795c1b5e1e2df7
Smit Panchal <smitkumar.panchal@toneop.com>
2023-12-10 11:49:15 +0530

    First commit in feature-2'

--- samplerepo/samplerepo/settings.py
+++ samplerepo/samplerepo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^9hs5z1v8%qni0t(35c)0$$no_3pycuc)4f@iz&ncrdrnl##ar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
